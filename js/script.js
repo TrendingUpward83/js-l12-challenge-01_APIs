bc9e4817c235aa712f252a79e1e490a94af6e4cf
Christina_Sereday <christina.sereday@gmail.com>
2023-11-11 19:24:20 -0500

    API connection established

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+
+const getImage = async function(){
+    const res = await fetch("https://picsum.photos/v2/list?limit=100");
+    const images = await res.json(); 
+    return images;
+};
+
+
+console.log(getImage()); 
