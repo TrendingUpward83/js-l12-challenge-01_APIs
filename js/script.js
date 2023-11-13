261cade688abfc16ea48f5ee19d842bd5755d0cc
Christina_Sereday <christina.sereday@gmail.com>
2023-11-12 21:49:17 -0500

    added get image and event listener

--- js/script.js
+++ js/script.js
@@ -8,14 +8,27 @@ const getImage = async function(){
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json(); 
     selectRandomImage(images);
-    return images;
+    
 };
+const displayImage= function(randomImage){
+    const author= randomImage.author;
+    const imageAddress = randomImage.download_url; 
+    authorSpan.innerText= author;
+    img.innerHTML= `img src="${imageAddress}"`;
+    imgDiv.classList.remove("hide"); 
+
+};
+
 
 const selectRandomImage = function (images){
     const randomIndex = Math.floor(Math.random()*images.length); 
     //console.log(randomIndex); 
-    const randomImages = images[randomIndex]; 
+    const randomImage = images[randomIndex]; 
+    console.log(randomImage);
+    displayImage(randomImage); 
 };
 
 
-console.log(getImage()); 
+button.addEventListener("click", function(){
+    getImage();
+});
