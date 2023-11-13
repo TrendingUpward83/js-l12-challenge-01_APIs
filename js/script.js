1b3cf532dfa6f854a0a29275df24653f740a19a1
Christina_Sereday <christina.sereday@gmail.com>
2023-11-12 20:43:15 -0500

    added random image selection

--- js/script.js
+++ js/script.js
@@ -7,7 +7,14 @@ const img = document.querySelector(".img");
 const getImage = async function(){
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json(); 
+    selectRandomImage(images);
     return images;
+};
+
+const selectRandomImage = function (images){
+    const randomIndex = Math.floor(Math.random()*images.length); 
+    //console.log(randomIndex); 
+    const randomImages = images[randomIndex]; 
 };
 
 
